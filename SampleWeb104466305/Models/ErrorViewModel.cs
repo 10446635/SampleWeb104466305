b100957ab16abe4aa4a5cbea947cee83d1a1222e
James G <cjgesthuizen@gmail.com>
2026-08-30 21:37:01 +1000

    Add project files.

---
+++ SampleWeb104466305/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleWeb104466305.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
